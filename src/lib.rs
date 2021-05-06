c5873481c83caf80e5479ecf0cc9e0b6cbb9cab2
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-05-06 10:17:05 +0100

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-mod event;
+//! A library implementation of the nostr protocol.
+//!
+//! nostr sources:
+//! - [nostr nips (nostr improvement proposals)](https://github.com/fiatjaf/nostr)
+mod error;
 
-pub use event::Event;
+mod event;
+mod util;
+
+pub use event::{Event, Kind, Tag};
+pub use util::unix_u32_now;
