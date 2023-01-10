bfc7055ed5c7180e7c29c74efb85796483b11d01
Christopher Coverdale <chris.coverdale24@gmail.com>
2023-01-10 22:21:43 +0000

    tags: add documentation

--- src/tags.rs
+++ src/tags.rs
@@ -1,5 +1,12 @@
+/// Alias for a vector of a vector of strings. The first element of each subarray
+/// is the tag identifier:
+///   - "p": pubkey, which points to a pubkey of someone that is referred to in the event
+///   - "e": event, which points to the id of an event this event is quoting, replying to or
+///   referring to somehow
 pub type Tags = Vec<Vec<String>>;
 
+/// Helper macro to convert a Vector<&str> to the formatting for the type Tags.
+#[macro_export]
 macro_rules! tags_from_vec_str {
     ($($tags:expr),*) => {
         vec![
